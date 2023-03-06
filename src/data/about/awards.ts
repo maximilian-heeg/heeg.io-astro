546b190cc2c29882d9d64979a4c5709d515137ce
Maximilian Heeg <maxheeg@gmail.com>
2023-03-06 15:11:43 -0800

    Added about page and aliases

---
+++ src/data/about/awards.ts
@@ -0,0 +1,24 @@
+export const awards = [
+  {
+    title: "La Jolla Immunology Conference Young Investigator Award",
+    description: "Oct 2022",
+  },
+  {
+    title: "Research Fellowships",
+    description: "German Research Foundation · Mar 2020 - Aug 2022",
+  },
+  {
+    title: "Fellow of the Berta-Ottenstein Programme for Clinician Scientists ",
+    description: "University Hospital Freiburg &#183; Oct 2016 – Sep 2019",
+  },
+  {
+    title:
+      "Scholarship for MD students of the Integrated Research and Training Group on Immunodeficiency",
+    description: "SFB620 &#183; Jan 2011",
+  },
+  {
+    title: "Scholar of the German National Academic Foundation",
+    description:
+      "German National Academic Foundation &#183; Jan 2009 – May 2014",
+  },
+];
---
+++ src/data/about/experience.ts
@@ -0,0 +1,36 @@
+export const experience = [
+  {
+    title: "PostDoc",
+    location: "University of California San Diego",
+    dates: "Jan 2020 – Present",
+    country: "California",
+    description: "DFG Research Fellowship",
+    project:
+      "Analysis of transcriptional regulators of tissue-resident memory differentiation",
+  },
+  {
+    title: "Fellow of the Berta-Ottenstein Programme for Clinician Scientists",
+    location: "University of Freiburg",
+    dates: "Oct 2016 – Sep 2019",
+    country: "Germany",
+    description: "",
+    project:
+      "Distinct molecular response patterns of activating STAT3 mutation associate with penetrance of lymphoproliferation and autoimmunity",
+  },
+  {
+    title: "Recidency at the Center for Chronic Immunodeficiency",
+    location: "University of Freiburg",
+    dates: "Apr 2016 – Jan 2020",
+    country: "Germany",
+    description: "",
+    project: "",
+  },
+  {
+    title: "Recidency in pediatrics",
+    location: "University of Freiburg",
+    dates: "Aug 2014 – Jan 2020",
+    country: "Germany",
+    description: "",
+    project: "",
+  },
+];
