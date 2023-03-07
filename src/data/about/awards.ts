22a74a6a9701d9037782b8536fc38fd46a16aa28
Maximilian Heeg <maxheeg@gmail.com>
2023-03-06 18:37:04 -0800

    Added Publications

--- src/data/about/awards.ts
+++ src/data/about/awards.ts
@@ -9,16 +9,16 @@ export const awards = [
   },
   {
     title: "Fellow of the Berta-Ottenstein Programme for Clinician Scientists ",
-    description: "University Hospital Freiburg &#183; Oct 2016 – Sep 2019",
+    description: "University Hospital Freiburg · Oct 2016 – Sep 2019",
   },
   {
     title:
       "Scholarship for MD students of the Integrated Research and Training Group on Immunodeficiency",
-    description: "SFB620 &#183; Jan 2011",
+    description: "SFB620 · Jan 2011",
   },
   {
     title: "Scholar of the German National Academic Foundation",
     description:
-      "German National Academic Foundation &#183; Jan 2009 – May 2014",
+      "German National Academic Foundation · Jan 2009 – May 2014",
   },
 ];
