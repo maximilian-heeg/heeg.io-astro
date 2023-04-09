24667cf96cac1af77231df70a5a8d729a729a43d
Maximilian Heeg <maxheeg@gmail.com>
2023-04-08 17:19:56 -0700

    Added basic support for galleries

---
+++ src/data/gallery.ts
@@ -0,0 +1,26 @@
+export const galleries = [
+  {
+    title: "Indie",
+    path: "indie",
+    description: "Pictures of my dog.",
+    images: [
+      { img: "PXL_20220121_010759027-EFFECTS", alt: "Indie is watching the sunset" },
+      { img: "PXL_20221026_031336479", alt: "Indie is presenting a scientific poster" },
+      { img: "PXL_20221031_205106214", alt: "Indie is working in the office" },
+      { img: "PXL_20221224_225300633", alt: "Indie is taking a bath" },
+      { img: "PXL_20230305_222051344", alt: "Indie enjoys the snow" },
+    ],
+  },
+  {
+    title: "Indie 2",
+    path: "indie",
+    description: "Pictures of my dog. Same photos, but different order",
+    images: [
+        { img: "PXL_20230305_222051344", alt: "Indie enjoys the snow" },
+        { img: "PXL_20221026_031336479", alt: "Indie is presenting a scientific poster" },
+        { img: "PXL_20221031_205106214", alt: "Indie is working in the office" },
+        { img: "PXL_20221224_225300633", alt: "Indie is taking a bath" },
+        { img: "PXL_20220121_010759027-EFFECTS", alt: "Indie is watching the sunset" },
+    ],
+  },
+];
--- src/data/navigation.ts
+++ src/data/navigation.ts
@@ -1,5 +1,6 @@
 export const links = [
   { text: "Home", url: "/" },
   { text: "Publications", url: "/publications/" },
+  // { text: "Gallery", url: "/gallery/" },
   { text: "About", url: "/about/" },
 ];
