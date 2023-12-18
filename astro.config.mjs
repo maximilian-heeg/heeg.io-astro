84a7e349c4105a66cb662b001541cf30db845f96
Max <maxheeg@gmail.com>
2023-12-17 19:22:47 -0800

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,11 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
-
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import purgecss from "astro-purgecss";
+
+import sitemap from "astro-sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://heeg.io",
-  integrations: [
-    UnoCSS(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-    purgecss(),
-  ],
+  integrations: [UnoCSS(), purgecss(), sitemap()]
 });
