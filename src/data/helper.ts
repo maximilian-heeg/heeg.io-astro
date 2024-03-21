bc81894eb8afd050eeb35efb6326d77b4530b43f
Max <maxheeg@gmail.com>
2024-03-20 22:06:50 -0700

    Added API Key

--- src/data/helper.ts
+++ src/data/helper.ts
@@ -1,28 +1,22 @@
 import axios, { AxiosError } from "axios";
 
 export async function makeRequest(url: string): Promise<any> {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    if ((error as AxiosError).response?.status === 429) {
-      // Retry logic
-      let retryCount = 0;
-      while (retryCount < 3) {
-        await new Promise((resolve) => setTimeout(resolve, 1000)); // Pause for 1 second
-        try {
-          const response = await axios.get(url);
-          return response.data;
-        } catch (error) {
-          if ((error as AxiosError).response?.status !== 429) {
-            throw error; // Re-throw error if it's not a 429 status
-          }
-        }
-        retryCount++;
+  let retryCount = 0;
+  while (retryCount < 4) {
+    try {
+      const response = await axios.get(url, {
+        headers: {
+          "x-api-key": import.meta.env.SEMANTIC_API_KEY,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      if ((error as AxiosError).response?.status !== 429) {
+        throw error; // Re-throw error if it's not a 429 status
       }
-      throw new Error("Max retries exceeded");
-    } else {
-      throw error; // Re-throw error if it's not a 429 status code
     }
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Pause for 1 second
+    retryCount++;
   }
+  throw new Error("Max retries exceeded");
 }
