10dbf1b1a99b2fb74d384a51ebaeb8915105cd2e
Max <maxheeg@gmail.com>
2024-03-22 18:26:36 -0700

    prepare for MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import UnoCSS from "unocss/astro";
 import purgecss from "astro-purgecss";
-
 import sitemap from "astro-sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://heeg.io",
-  integrations: [UnoCSS(), purgecss(), sitemap()],
+  integrations: [UnoCSS(), purgecss(), sitemap(), mdx()]
 });
