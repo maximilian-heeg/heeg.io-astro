7a661a9fd262863ecc8c33e60615cecb26cda0cc
Max <maxheeg@gmail.com>
2024-07-10 17:11:09 -0700

    Css update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,4 @@
-// 1. Import utilities from `astro:content`
-import { defineCollection } from 'astro:content';
+// 1. Import
 import {blogSchema} from './schema.ts';
 // 2. Define your collection(s)
 const blogCollection = blogSchema;
